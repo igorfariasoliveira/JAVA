ec5a80c06a14507bf0b5de64ff85eb74917b8eb4
igorfariasoliveira <igor10farias@gmail.com>
2023-10-31 09:31:19 -0300

    Programa matematica

---
+++ Inicio/ProgramaMath.java
@@ -0,0 +1,28 @@
+public class ProgramaMath {
+  public static void main (String entrada[])
+  {
+    int n1, n2;
+    int mod, div;
+    double raiz, pot;
+    String msg = "";
+
+    n1 = Integer.parseInt(entrada[0]);
+    n2 = Integer.parseInt(entrada[1]);
+          
+    mod = n1 % n2;
+    div = (int)n1 / (int)n2;
+    raiz = Math.sqrt(n1);
+    pot = Math.pow(n1, n2);
+
+    msg = msg + "n1 = " + n1 + " ne = " + n2 + "\n";
+    msg = msg + "resto da divisao de n1 por n2 = " + mod + "\n";
+    msg = msg + "quociente da divisao de n1 por n2 = " + div + "\n";
+    msg = msg + "raiz qudrada de n1 = " + raiz + "\n";
+    msg = msg + "potencia de n1 por n2 = " + pot + "\n";
+
+    System.out.println(msg);
+
+    System.exit(0);
+
+  }
+}
