9f690347d04aa8c5326005efeb5835e835195288
igorfariasoliveira <igor10farias@gmail.com>
2023-11-16 08:13:06 -0300

    Update CalcularIdade.java

--- Inicio/calcularidade/CalcularIdade.java
+++ Inicio/calcularidade/CalcularIdade.java
@@ -1,14 +1,19 @@
 package calcularidade;
 import javax.swing.JOptionPane;
+import java.time.LocalDate;
 
 public class CalcularIdade {
   public static void main(String[] args) {
-    int idade;
+    int idade,nascimento;
     String nome;
+    int anoAtual = LocalDate.now().getYear();
 
     nome = JOptionPane.showInputDialog("Qual o seu nome?");
-    idade = Integer.parseInt(JOptionPane.showInputDialog("Em que ano você nasceu?"));
+    nascimento = Integer.parseInt(JOptionPane.showInputDialog("Em que ano você nasceu?"));
     
     
+    idade = anoAtual-nascimento;
+    
+    JOptionPane.showMessageDialog(null, "Olá, " +nome+"! Você tem "+idade+" anos");
   }
 }
