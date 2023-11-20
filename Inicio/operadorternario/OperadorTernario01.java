07f1f788b4a2ed24122b98e8dde58472cb31b05d
igorfariasoliveira <igor10farias@gmail.com>
2023-11-20 08:01:57 -0300

    Create OperadorTernario01.java

---
+++ Inicio/operadorternario/OperadorTernario01.java
@@ -0,0 +1,12 @@
+package operadorternario;
+public class OperadorTernario01 {
+  public static void main(String[] args) {
+    int n1, n2, r;
+
+    n1=2;
+    n2=5;
+    r = n1>n2?0:1;
+
+    System.out.println(r);
+  }
+}
