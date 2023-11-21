690fd1baa4d78807e6cf31b9e9cdb3d8124b36b7
igorfariasoliveira <igor10farias@gmail.com>
2023-11-21 11:21:51 -0300

    Update CalcularIdade.java

--- Inicio/estruturascondicionais/CalcularIdade.java
+++ Inicio/estruturascondicionais/CalcularIdade.java
@@ -4,9 +4,15 @@ import javax.swing.JOptionPane;
 
 public class CalcularIdade {
   public static void main(String[] args) {
-    int idade, ano, nasc;
+    int idade, nasc;
 
     nasc = Integer.parseInt(JOptionPane.showInputDialog("Em que ano você nasceu?"));
+    idade = 2023-nasc;
 
+    if (idade >= 18) {
+      JOptionPane.showMessageDialog(null, "Você é maior de idade. Tem "+idade+" anos");
+    }else{
+      JOptionPane.showMessageDialog(null, "Você é menor de idade. Tem "+idade+" anos");
+    }
   }
 }
