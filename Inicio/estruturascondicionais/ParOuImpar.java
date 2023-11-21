fe1e56b7c071aafbfd9da6c4b35b038f5d44f59f
igorfariasoliveira <igor10farias@gmail.com>
2023-11-21 11:28:24 -0300

    Create ParOuImpar.java

---
+++ Inicio/estruturascondicionais/ParOuImpar.java
@@ -0,0 +1,11 @@
+package estruturascondicionais;
+
+import javax.swing.JOptionPane;
+
+public class ParOuImpar {
+  public static void main(String[] args) {
+    int n1,res;
+
+    n1 = Integer.parseInt(JOptionPane.showInputDialog("Digite um número!"));
+  }
+}
