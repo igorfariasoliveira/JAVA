9d4e643d5217db6497f290d30ef10d2752495865
igorfariasoliveira <igor10farias@gmail.com>
2023-11-21 09:28:41 -0300

    Create CalcularIdade.java

---
+++ Inicio/estruturascondicionais/CalcularIdade.java
@@ -0,0 +1,12 @@
+package estruturascondicionais;
+
+import javax.swing.JOptionPane;
+
+public class CalcularIdade {
+  public static void main(String[] args) {
+    int idade, ano, nasc;
+
+    nasc = Integer.parseInt(JOptionPane.showInputDialog("Em que ano você nasceu?"));
+
+  }
+}
