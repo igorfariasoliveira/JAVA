02b05d3488ad83c2734e0d87c206639cf77a9814
igorfariasoliveira <igor10farias@gmail.com>
2023-11-22 08:36:47 -0300

    Update EquacaoSegundoGrau.java

--- Inicio/estruturascondicionais/EquacaoSegundoGrau.java
+++ Inicio/estruturascondicionais/EquacaoSegundoGrau.java
@@ -1,5 +1,21 @@
 package estruturascondicionais;
+import javax.swing.JOptionPane;
+
 
 public class EquacaoSegundoGrau {
-  
+  public static void main(String[] args) {
+    int na,nb,nc,delta;
+    
+    JOptionPane.showMessageDialog(null, "Escreva os valores de A, B e C da equação do segundo grau");
+    na = Integer.parseInt(JOptionPane.showInputDialog("Valor de A"));
+    nb = Integer.parseInt(JOptionPane.showInputDialog("Valor de B"));
+    nc = Integer.parseInt(JOptionPane.showInputDialog("Valor de C"));
+
+    delta = (nb*nb) - (4*na*nc);
+    if (delta >= 0) {
+      JOptionPane.showMessageDialog(null, delta+"\nExistem raizes reais");
+    } else{
+      JOptionPane.showMessageDialog(null, delta+"\nNão existem raizes reais");
+    }
+  }
 }
