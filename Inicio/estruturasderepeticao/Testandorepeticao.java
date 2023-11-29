34c5cf0b5800083f721f8222258c6ca81f884444
igorfariasoliveira <igor10farias@gmail.com>
2023-11-29 08:43:28 -0300

    Create Testandorepeticao.java

---
+++ Inicio/estruturasderepeticao/Testandorepeticao.java
@@ -0,0 +1,12 @@
+package estruturasderepeticao;
+
+public class Testandorepeticao {
+  public static void main(String[] args) {
+    int carneirinhos=0;
+
+    while (carneirinhos<10) {
+      System.out.println(carneirinhos + " carneirinhos");
+      carneirinhos++;
+    }
+  }
+}
