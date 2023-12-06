49b184435c9efd91b5c5b3d104ce41674f6c68da
igorfariasoliveira <igor10farias@gmail.com>
2023-12-06 14:04:56 -0300

    Update EscolhendoNumeros.java

--- Inicio/estruturasderepeticao/EscolhendoNumeros.java
+++ Inicio/estruturasderepeticao/EscolhendoNumeros.java
@@ -6,9 +6,10 @@ public class EscolhendoNumeros {
   public static void main(String[] args) {
     int numb, pares = 0, impares = 0, acima = 0, media;
     int c=0;
+    int somaValores=0;
     do {
       numb = Integer.parseInt(JOptionPane.showInputDialog("Escolha um número! Se quiser interromper aperte 0"));
-      if (numb % 2 == 0) {
+      if (numb % 2 == 0 && numb!=0) {
         pares++;
       }
       if (numb % 2 != 0) {
@@ -17,11 +18,13 @@ public class EscolhendoNumeros {
       if(numb > 100){
         acima++;
       }
-      c++;
-
+      if (numb != 0) {
+        c++;
+        somaValores+=numb;
+      }
     } while(numb!=0);
 
-    media = numb/c;
-    JOptionPane.showMessageDialog(null, "Resultados: \n"+"Toral de valore:" +c+"\n" + "Total de pares: "+ pares + "\n"+"total de impares");
+    media = somaValores/c;
+    JOptionPane.showMessageDialog(null, "Resultados: \n"+"Total de valore: " +c+"\n" + "Total de pares: "+ pares + "\n"+"total de impares: " +impares+ "\n"+ "Acima de 100: "+acima+"\n"+"Média dos valores: "+media);
   }
 }
