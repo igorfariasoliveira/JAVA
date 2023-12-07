fc26975bada0f2d856bcc1312aae263b0a0ae8de
igorfariasoliveira <igor10farias@gmail.com>
2023-12-07 15:57:44 -0300

    Update ForComEscolhas.java

--- Inicio/estruturasderepeticao/ForComEscolhas.java
+++ Inicio/estruturasderepeticao/ForComEscolhas.java
@@ -5,13 +5,16 @@ import javax.swing.JOptionPane;
 public class ForComEscolhas {
   public static void main(String[] args) {
     int inicio, fim, passo;
+    String result="";
 
     inicio = Integer.parseInt(JOptionPane.showInputDialog("Escolha o inicio"));
     fim= Integer.parseInt(JOptionPane.showInputDialog("Escolha o fim"));
     passo = Integer.parseInt(JOptionPane.showInputDialog("Escolha a quantidade de numeros que ira pular as casas"));
 
     for(int c=inicio;c<=fim;c+=passo){
-      JOptionPane.showMessageDialog(null, c);
-    }
+      result+=c+"\n";
+    };
+    
+    JOptionPane.showMessageDialog(null, result);
   }
 }
