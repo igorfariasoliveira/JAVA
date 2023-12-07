2d994ce45b2c6c0a048f0250f81f55491f103f01
igorfariasoliveira <igor10farias@gmail.com>
2023-12-07 13:46:45 -0300

    Update RepFor.java

--- Inicio/estruturasderepeticao/RepFor.java
+++ Inicio/estruturasderepeticao/RepFor.java
@@ -2,7 +2,7 @@ package estruturasderepeticao;
 
 public class RepFor {
   public static void main(String[] args) {
-    for(int cc=1;cc<=10;cc++){
+    for(int cc=0;cc<=100;cc+=10){
       System.out.println(cc);
     }
   }
