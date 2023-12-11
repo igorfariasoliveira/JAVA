a2fde8ea9351a284b73cd4e0d81baa2fc0b9ca2f
igorfariasoliveira <igor10farias@gmail.com>
2023-12-11 14:44:31 -0300

    Update Vetores01.java

--- Inicio/Vetores/Vetores01.java
+++ Inicio/Vetores/Vetores01.java
@@ -4,8 +4,9 @@ public class Vetores01 {
   public static void main(String[] args) {
     int n[]= {1,5,2,9,8};
 
-    for (int c=0; c<=5; c++){
+    for (int c=0; c<=n.length-1; c++){
       System.out.println("Na posição "+c+" temos o valor "+n[c]);
+      
     }
   }
 }
