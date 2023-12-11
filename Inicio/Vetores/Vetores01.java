829e79c556bfe71b645b2c927616611a52654001
igorfariasoliveira <igor10farias@gmail.com>
2023-12-11 14:43:30 -0300

    Create Vetores01.java

---
+++ Inicio/Vetores/Vetores01.java
@@ -0,0 +1,11 @@
+package Vetores;
+
+public class Vetores01 {
+  public static void main(String[] args) {
+    int n[]= {1,5,2,9,8};
+
+    for (int c=0; c<=5; c++){
+      System.out.println("Na posição "+c+" temos o valor "+n[c]);
+    }
+  }
+}
