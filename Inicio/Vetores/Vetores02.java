b377562021cfae3166cffd98d1381d1889c17005
igorfariasoliveira <igor10farias@gmail.com>
2023-12-12 08:19:16 -0300

    Update Vetores02.java

--- Inicio/Vetores/Vetores02.java
+++ Inicio/Vetores/Vetores02.java
@@ -1,9 +1,12 @@
 package Vetores;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Vetores02 {
   public static void main(String[] args) {
     int num[] = {8,5,2,6,4};
-
+    Arrays.sort(num);
     for(int cont: num){
       System.out.println(cont);
     }
