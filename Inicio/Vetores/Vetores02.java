c7fd3b52433a805f6951f416427fc9967e34a17c
igorfariasoliveira <igor10farias@gmail.com>
2023-12-12 08:14:35 -0300

    Create Vetores02.java

---
+++ Inicio/Vetores/Vetores02.java
@@ -0,0 +1,11 @@
+package Vetores;
+
+public class Vetores02 {
+  public static void main(String[] args) {
+    int num[] = {8,5,2,6,4};
+
+    for(int cont: num){
+      System.out.println(cont);
+    }
+  }
+}
