0b181ba2f5bf68f4a6b4208fd49cfce6ba4cb3c0
igorfariasoliveira <igor10farias@gmail.com>
2023-12-12 08:28:11 -0300

    Update Vetores02.java

--- Inicio/Vetores/Vetores02.java
+++ Inicio/Vetores/Vetores02.java
@@ -7,8 +7,12 @@ public class Vetores02 {
   public static void main(String[] args) {
     int num[] = {8,5,2,6,4};
     Arrays.sort(num);
+    int p = Arrays.binarySearch(num,8);
+    System.out.println(p);
+
+    /*Arrays.sort(num);
     for(int cont: num){
       System.out.println(cont);
-    }
+    }*/
   }
 }
