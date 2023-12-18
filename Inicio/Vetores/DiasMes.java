03a14afd7a434ea1535c9dd6e083619a60e73f24
igorfariasoliveira <igor10farias@gmail.com>
2023-12-18 07:49:10 -0300

    Update DiasMes.java

--- Inicio/Vetores/DiasMes.java
+++ Inicio/Vetores/DiasMes.java
@@ -4,10 +4,12 @@ import javax.swing.JOptionPane;
 
 public class DiasMes {
   public static void main(String[] args) {
+    
     String mes[] = {"Jan","Fev","Mar","Abr","Mai","Jun","Jul","Ago","Set","Out","Nov","Dez"};
     int ano;
 
     ano = Integer.parseInt(JOptionPane.showInputDialog("Qual ano quer consultar?"));
+    
     if (ano%4==0) {
       String dias[] = {"31","29","31","30","31","30","31","31","30","31","30","31",};
       for(int c=0; c<mes.length; c++){
@@ -19,6 +21,5 @@ public class DiasMes {
       System.out.println("O mês "+mes[c]+" tem "+dias[c]+" dias");
     }
     }
-    
   }
 }
