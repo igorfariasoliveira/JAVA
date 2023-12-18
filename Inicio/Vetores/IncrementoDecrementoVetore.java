98f49d59fffa63e2421a5bdb17f0a671f1cc9791
igorfariasoliveira <igor10farias@gmail.com>
2023-12-18 08:23:44 -0300

    Update IncrementoDecrementoVetore.java

--- Inicio/Vetores/IncrementoDecrementoVetore.java
+++ Inicio/Vetores/IncrementoDecrementoVetore.java
@@ -6,12 +6,18 @@ public class IncrementoDecrementoVetore {
   public static void main(String[] args) {
     
     int v[] = new int[10];
-    String adicionar, remover, organizar;
+    String adicionar, remover, organizar, escolha;
 
     for (int i = 0; i < 10; i++){
       v[i] = Integer.parseInt(JOptionPane.showInputDialog("Escolha um número do vetor"));
     }
+    
+    StringBuilder mensagem = new StringBuilder("Vetor: ");
+    for (int elemento : v) {
+        mensagem.append(elemento).append(" ");
+    }
 
-    JOptionPane.showMessageDialog(null, v);
+    JOptionPane.showMessageDialog(null, mensagem.toString());
+    escolha = JOptionPane.showInputDialog("Gostaria de editar seu vetor?");
   }
 }
