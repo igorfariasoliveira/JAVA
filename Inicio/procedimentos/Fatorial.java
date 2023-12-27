884b3a61ba0ed009cb6a2b03294f9f28551b6a17
igorfariasoliveira <igor10farias@gmail.com>
2023-12-27 08:41:45 -0300

    Update Fatorial.java

--- Inicio/procedimentos/Fatorial.java
+++ Inicio/procedimentos/Fatorial.java
@@ -3,5 +3,18 @@ package procedimentos;
 public class Fatorial {
   
   private int num;
-  private int for
+  private int fat;
+  private String formula = "";
+
+  public void setValor(int n){
+    num = n;
+    int f = 1;
+    String s = "";
+    for(int c = n; c > 1; c--){
+      f *= c;
+      s += c + " x ";
+    }
+    s += "1 = ";
+    
+  }
 }
