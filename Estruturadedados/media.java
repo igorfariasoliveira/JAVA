1f879f519b79964c32928e2e0f5dd0f9aa83a660
igorfariasoliveira <igor10farias@gmail.com>
2024-08-08 10:44:32 -0300

    atividade

--- Estruturadedados/media.java
+++ Estruturadedados/media.java
@@ -1,15 +1,18 @@
 import javax.swing.*;
-public class vetor {
+public class media {
   public static void main(String args []) {
     int VetSoma [] , i , soma = 0;
-    VetSoma = new int[100];
+    double media = 0;
+    VetSoma = new int[50];
 
     for (i = 0; i <= 49; i++){
       VetSoma[i] = Integer.parseInt(JOptionPane.showInputDialog("Digite um valor inteiro"));
       soma = soma + VetSoma[i];
       System.out.println(i);
     } 
+    media = (double) soma/50;
     System.out.println("A soma dos valores é: "+soma);
+    System.out.println("A média desses valores é: "+media);
     System.exit(0);
   }
 }
