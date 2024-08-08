8ba0a0ae7aee113aafe8b75855a58291ddea16a8
igorfariasoliveira <igor10farias@gmail.com>
2024-08-08 10:25:03 -0300

    aula

---
+++ Estruturadedados/vetor.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+public class vetor {
+  public static void main(String args []) {
+    int VetSoma [] , i , soma = 0;
+    VetSoma = new int[100];
+
+    for (i = 0; i <= 9; i++){
+      VetSoma[i] = Integer.parseInt(JOptionPane.showInputDialog("Digite um valor inteiro"));
+      soma = soma + VetSoma[i];
+      System.out.println(i);
+    } 
+    System.out.println("A soma dos valores é: "+soma);
+    System.exit(0);
+  }
+}
