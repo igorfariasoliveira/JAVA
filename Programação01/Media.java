126487b2bc8fbb8115867c720355995f97a23447
igorfariasoliveira <igor10farias@gmail.com>
2024-08-20 11:48:49 -0300

    Create Media.java

---
+++ Programação01/Media.java
@@ -0,0 +1,13 @@
+package Programação01;
+
+import java.util.Scanner;
+
+public class Media {
+  
+  public static void main(String[] args) {
+    
+    Scanner input = new Scanner(System.in);
+    
+    double nota01, nota02, nota03, media;
+  }
+}
