53fdbd5a4d2163dc0d90be312b15cb7033f1ea9c
igorfariasoliveira <igor10farias@gmail.com>
2024-08-22 08:10:39 -0300

    Create Professor.java

---
+++ Programacao01/Professor.java
@@ -0,0 +1,25 @@
+package Programacao01;
+
+public class Professor {
+  String name;
+  double salario;
+  
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public double getSalario() {
+    return salario;
+  }
+  public void setSalario(double salario) {
+    this.salario = salario;
+  }
+
+  public double getInss(){
+    
+  }
+
+  
+}
