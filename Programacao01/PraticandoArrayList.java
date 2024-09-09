30f3b9a8b6085d23fc017e1c9b4aa0f12677fbc1
igorfariasoliveira <igor10farias@gmail.com>
2024-09-09 08:16:07 -0300

    Create PraticandoArrayList.java

---
+++ Programacao01/PraticandoArrayList.java
@@ -0,0 +1,18 @@
+package Programacao01;
+
+import java.util.ArrayList;
+
+public class PraticandoArrayList {
+  public static void main(String[] args) {
+    
+    ArrayList<String> times = new ArrayList<>();
+
+    times.add("Flamengo");
+    times.add("Grêmio");
+    times.add("Vasco");
+    times.add("Palmeiras");
+    times.add("Fortaleza");
+
+    
+  }
+}
