1e02682ca5f6828c71149dc96baa176cc5d18bdf
igorfariasoliveira <igor10farias@gmail.com>
2024-09-09 09:59:02 -0300

    Create AtividadeNumeros.java

---
+++ Programacao01/AtividadeNumeros.java
@@ -0,0 +1,13 @@
+package Programacao01;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AtividadeNumeros {
+  public static void main(String[] args) {
+    
+    Scanner input = new Scanner(System.in);
+
+    ArrayList<int>
+  }
+}
