5bbc172fd3134a777b670ad87d87e4d1693cf893
igorfariasoliveira <igor10farias@gmail.com>
2024-09-09 08:19:58 -0300

    Update PraticandoArrayList.java

--- Programacao01/PraticandoArrayList.java
+++ Programacao01/PraticandoArrayList.java
@@ -13,6 +13,15 @@ public class PraticandoArrayList {
     times.add("Palmeiras");
     times.add("Fortaleza");
 
+    System.out.println("Antes");
+
+    for(String nome: times){
+      System.out.println(nome);
+    }
     
+    times.set(1, "Internacional");
+
+    
+  
   }
 }
